732be5429fb39691560035a724d7d5f1df9aa569
Alexis RIGOULOT <alexis.rigoulot@2bsystem.fr>
2022-10-12 10:45:54 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+
+type="text/javascript"
+     gsap.timeline({
+        scrollTrigger: {
+            trigger: ".box",
+            start: "center center", //Animation start at this point
+            end: "bottom top", //Animation end at this point
+            markers : true,
+            scrub : true,
+            pin : true,
+        }
+     })
+    .from(".text1", { x : innerWidth * 1})
+    .from(".text2", { x : innerWidth * -1})
+    .from(".text3", { x : innerWidth * 1})
+    .from(".logo", { y : innerHeight * 1, rotate : 180})
+
+
+
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: ".box2",
+            start: "center center", //Animation start at this point
+            end: "bottom top", //Animation end at this point
+            markers : true,
+            scrub : true,
+            pin : true,
+        }
+     })
+    .from(".box2", { opacity : 0 })
+    .from(".text4", { y : innerHeight * 1})
+    .from(".text5", { y : innerHeight * 1})
+    .from(".text6", { y : innerHeight * 1})
